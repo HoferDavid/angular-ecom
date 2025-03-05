0e8c0cc8829daea0b71ab2bb2e35730525aacc7c
David Hofer <chassis.poppets0x@icloud.com>
2025-03-05 16:36:00 -0300

    feat: add product-card component

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Product } from '../../models/products.models';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
 
+  products = signal<Product[]>([
+    {
+      id: 1,
+      title: 'string',
+      image: 'string',
+      price: 109.95,
+      stock: 10,
+    },
+    {
+      id: 2,
+      title: 'stringtwo',
+      image: 'stringtwo',
+      price: 119.95,
+      stock: 11,
+    },
+    {
+      id: 3,
+      title: 'stringthree',
+      image: 'stringthree',
+      price: 129.95,
+      stock: 13,
+    },
+    {
+      id: 4,
+      title: 'stringfour',
+      image: 'stringfour',
+      price: 149.95,
+      stock: 14,
+    },
+  ])
 }
