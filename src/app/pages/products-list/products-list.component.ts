05583cbfae4313f49685a950240a9b7ed9a314a0
David Hofer <chassis.poppets0x@icloud.com>
2025-03-06 11:49:42 -0300

    feat: change products list to api fetch

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -10,34 +10,40 @@ import { ProductCardComponent } from './product-card/product-card.component';
 })
 export class ProductsListComponent {
 
+  async ngOnInit() {
+    const res = await fetch('https://fakestoreapi.com/products/category/electronics');
+    const data = await res.json();
+    this.products.set(data);
+  }
+
   products = signal<Product[]>([
-    {
-      id: 1,
-      title: 'string',
-      image: 'https://picsum.photos/seed/picsum/200/300',
-      price: 109.95,
-      stock: 0,
-    },
-    {
-      id: 2,
-      title: 'stringtwo',
-      image: 'https://picsum.photos/seed/picsum/200/300',
-      price: 119.95,
-      stock: 11,
-    },
-    {
-      id: 3,
-      title: 'stringthree',
-      image: 'https://picsum.photos/seed/picsum/200/300',
-      price: 129.95,
-      stock: 13,
-    },
-    {
-      id: 4,
-      title: 'stringfour',
-      image: 'https://picsum.photos/seed/picsum/200/300',
-      price: 149.95,
-      stock: 14,
-    },
+    // {
+    //   id: 1,
+    //   title: 'string',
+    //   image: 'https://picsum.photos/seed/picsum/200/300',
+    //   price: 109.95,
+    //   stock: 0,
+    // },
+    // {
+    //   id: 2,
+    //   title: 'stringtwo',
+    //   image: 'https://picsum.photos/seed/picsum/200/300',
+    //   price: 119.95,
+    //   stock: 11,
+    // },
+    // {
+    //   id: 3,
+    //   title: 'stringthree',
+    //   image: 'https://picsum.photos/seed/picsum/200/300',
+    //   price: 129.95,
+    //   stock: 13,
+    // },
+    // {
+    //   id: 4,
+    //   title: 'stringfour',
+    //   image: 'https://picsum.photos/seed/picsum/200/300',
+    //   price: 149.95,
+    //   stock: 14,
+    // },
   ])
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,36 +6,7 @@ import { Product } from '../models/products.models';
 })
 export class CartService {
 
-  cart = signal<Product[]>([
-    {
-      id: 1,
-      title: 'string',
-      image: 'https://picsum.photos/seed/picsum/200/300',
-      price: 109.95,
-      stock: 0,
-    },
-    {
-      id: 2,
-      title: 'stringtwo',
-      image: 'https://picsum.photos/seed/picsum/200/300',
-      price: 119.95,
-      stock: 11,
-    },
-    {
-      id: 3,
-      title: 'stringthree',
-      image: 'https://picsum.photos/seed/picsum/200/300',
-      price: 129.95,
-      stock: 13,
-    },
-    {
-      id: 4,
-      title: 'stringfour',
-      image: 'https://picsum.photos/seed/picsum/200/300',
-      price: 149.95,
-      stock: 14,
-    },
-  ]);
+  cart = signal<Product[]>([]);
 
   addToCart(product: Product) {
     this.cart.set([...this.cart(), product]);
